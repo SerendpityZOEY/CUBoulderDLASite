98ed6a79aa2768804b23077ec4ba1245edc2cba9
SerendpityZOEY <zoey.zhang513@hotmail.com>
2016-10-07 18:52:19 -0600

    link to MYsql

---
+++ app/static/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by yue on 10/7/16.
+ */
+$(function() {
+    $('#form1').click(function() {
+
+        $.ajax({
+            url: '/submit',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log($('form').serialize());
+                console.log(error);
+            }
+        });
+    });
+});
--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, request, json
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'SEDB'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 @app.route('/index')
@@ -11,6 +21,26 @@ def index():
 @app.route('/student')
 def student():
     return render_template("student.html")
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    name = request.form['name']
+    gender = request.form['optradio']
+    origin = request.form['optionsRadios']
+    race = request.form.getlist('hello')
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    query = "INSERT INTO `Demographic` (`name`, `gender`, `origin`, `race`) VALUES (%s,%s,%s,%s);"
+    print(query)
+    cursor.execute(query,(name,gender,origin,race))
+    conn.commit()
+    # cursor.callproc('sp_createUser', (_name, _gender, _origin, _race))
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return json.dumps({'message':'User created successfully !'})
 
 
 @app.route('/faculty')
