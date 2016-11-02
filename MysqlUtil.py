3d2af7908a876ab2c6e0cd93716206270636a17b
PeicheTsao <peichetsao@gmail.com>
2016-11-02 17:01:59 -0600

    add use_user and use_database support to MysqlUtil

--- MysqlUtil.py
+++ MysqlUtil.py
@@ -18,17 +18,37 @@ class MysqlUtil(object):
     def __init__(self, app):
         mysql = MySQL()
 
+        self.password = {
+            'user': 'l74z3oC1=1V>5J7',
+            'developer': 'nvSEXvXXUU9E2QFu',
+        }
+
+        self.app = app
+
         # MySQL configurations
-        app.config['MYSQL_DATABASE_USER'] = 'user'
-        app.config['MYSQL_DATABASE_PASSWORD'] = 'l74z3oC1=1V>5J7'
-        app.config['MYSQL_DATABASE_DB'] = 'SEDB'
         app.config['MYSQL_DATABASE_HOST'] = '54.186.181.45'
         app.config['MYSQL_DATABASE_PORT'] = 3306
+
+        self.use_account()
+        self.use_database()
+
         mysql.init_app(app)
 
         # Members variables initialization
         self.mysql = mysql
         self.data = {}
+
+
+    def use_account(self, username = 'user'):
+        if username not in self.password.keys():
+            print "Invalid Username"
+            username = 'user'
+        self.app.config['MYSQL_DATABASE_USER'] = username
+        self.app.config['MYSQL_DATABASE_PASSWORD'] = self.password[username]
+
+    def use_database(self, dbName = 'SEDB'):
+        self.app.config['MYSQL_DATABASE_DB'] = dbName
+
 
     def insert_push(self, field_name, record_value):
         """Add pair of key and value for one cell
