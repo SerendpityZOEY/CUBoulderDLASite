e3bd6e8e734b344e2eed4870955d1643127e9829
PeicheTsao <peichetsao@gmail.com>
2016-11-02 17:09:29 -0600

    Add doctrings to two functions, fix conflicts

--- MysqlUtil.py
+++ MysqlUtil.py
@@ -18,6 +18,9 @@ class MysqlUtil(object):
     def __init__(self, app):
         mysql = MySQL()
 
+        # Members variables initialization
+        self.mysql = mysql
+        self.data = {}
         self.password = {
             'user': 'l74z3oC1=1V>5J7',
             'developer': 'nvSEXvXXUU9E2QFu',
@@ -26,33 +29,39 @@ class MysqlUtil(object):
         self.app = app
 
         # MySQL configurations
-<<<<<<< HEAD
-=======
-        app.config['MYSQL_DATABASE_USER'] = 'root'
-        app.config['MYSQL_DATABASE_PASSWORD'] = 'l74z3oC1=1V>5J7'
-        app.config['MYSQL_DATABASE_DB'] = 'SEDB'
->>>>>>> 4de2fec536772231ee693147d55fc3a0b68bfff5
         app.config['MYSQL_DATABASE_HOST'] = '54.186.181.45'
         app.config['MYSQL_DATABASE_PORT'] = 3306
 
-        self.use_account()
-        self.use_database()
+        self.use_account('user')
+        self.use_database('SEDB')
 
         mysql.init_app(app)
 
-        # Members variables initialization
-        self.mysql = mysql
-        self.data = {}
-
 
     def use_account(self, username = 'user'):
+        """Specify the user to login
+
+        Args:
+            dbName (string): name of the user to use, default as 'user'
+
+        Returns:
+            -1: if the username not found in the lookup dictionary
+
+        """
         if username not in self.password.keys():
             print "Invalid Username"
             username = 'user'
         self.app.config['MYSQL_DATABASE_USER'] = username
         self.app.config['MYSQL_DATABASE_PASSWORD'] = self.password[username]
 
+
     def use_database(self, dbName = 'SEDB'):
+        """Specify the database to use
+
+        Args:
+            dbName (string): name of the database to use, default as 'SEDB'
+
+        """
         self.app.config['MYSQL_DATABASE_DB'] = dbName
 
 
@@ -65,6 +74,7 @@ class MysqlUtil(object):
 
         """
         self.data[field_name] = record_value
+
 
     def insert_execute(self, table_name):
         """Concatenate to get a query string and execute the inserting
@@ -91,9 +101,11 @@ class MysqlUtil(object):
         finally:
             connection.close()
 
+
     def clear(self):
         """Clear the pushed data"""
         self.data = {}
+
 
 # """ Example """
 # from app import app
